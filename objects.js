7a8096fcad48e8f6d7541c74188cfc540d85f2fb
Constance Goh <constgoh@gmail.com>
2017-12-30 08:31:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,6 @@
 var playlist = new Object({keys: '0'});
 
-function updatePlaylist{ (playlist, artistName, songTitle) 
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
