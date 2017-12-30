b435d96b4f25a3cc14cc3ad60ac1dc7281d8431a
Constance Goh <constgoh@gmail.com>
2017-12-30 08:34:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,4 +5,7 @@ function updatePlaylist (playlist, artistName, songTitle) {
   return playlist
 }
 
-function 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+  return playlist
+}
